b156c116fe4f3a823c3a2866e8f934edfd335e02
Michael Mimo Moratti <mimo@mimo.ch>
2012-04-08 12:29:09 +0200

    first initial py client

--- ParallaxController/control.py
+++ ParallaxController/control.py
@@ -1,16 +1,95 @@
-#!/usr/local/bin/python
+#!/usr/bin/env python3
 
-#import serial
-from subprocess import check_call
+import sys, subprocess, serial, json, threading, time
 
+DEFAULT_BAUDRATE = 9600
+DEFAULT_ENCODING = 'UTF-8'
+DEFAULT_EOF = '\n'
+
+
+#----------------------------------------------------------------------
 def main():
-	takePicture()
+	import optparse
+	
+	parser = optparse.OptionParser(
+		usage = "%prog [options] [port [baoudrate]]",
+		description = "Controller program for the orchestration between Arduino and gphoto2")
+		
+	parser.add_option("-d", "--device",
+		dest = "device",
+		help = "device, the device (tty.*) at which the Arduino is attached")
+		
+	parser.add_option("-b", "--baudrate",
+		dest = "baudrate",
+		help = "set baud rate, default %default",
+		default = DEFAULT_BAUDRATE)
+		
+	(options, args) = parser.parse_args()
+	
+	
+	
+	#takePicture()
+	sendCommand()
 
-
+#----------------------------------------------------------------------
 def takePicture():
 	try:
-		check_call(["gphoto2","-v"])
+		subprocess.check_call(["gphoto2","-v"])
 	except subprocess.CalledProcessError as e:
-		print "Error occured while attempting to take image!"
-		print type(e)
-		print e
+		sys.stderr.write("Error occured while attempting to take image!" % (e))
+		#print ("Error occured while attempting to take image!")
+		#print (type(e))
+		#print (e)
+	
+def sendCommand():
+	arduino = Arduino('/dev/tty.usbmodemfd3311',9600,1)
+	arduino.connect()
+	
+	request = json.dumps({"command":"initialize","port":"2","total-steps":"200","step-angle":"1.80","max-speed":"20","acceleration":"10"})
+
+	response = arduino.sendCommand(request)
+	
+	responseObject = json.loads(response)
+	sys.stdout.write(responseObject["status"] + DEFAULT_EOF)
+	
+	arduino.disconnect()
+
+#
+# Arduino communciations class
+# This class will run in it's own thread in order to send and receive data.
+#
+class Arduino(threading.Thread):
+	def __init__(self, device, baudrate, timeout):
+		threading.Thread.__init__(self)
+		self.port = serial.Serial(device,baudrate,timeout=timeout)
+		self.hasReceivedResponse = False
+		self.stopProcessing = False
+		self.response = bytearray();
+	
+	def run(self):
+		while (self.stopProcessing != True):
+			if(self.hasReceivedResponse == False):
+				self.response = self.port.readline()
+				if(len(self.response) > 0):
+					self.hasReceivedResponse = True
+	
+	def connect(self):
+		self.port.open()
+		self.start()
+		time.sleep(1)
+	
+	def disconnect(self):
+		self.stopProcessing = True
+		self.port.close()
+	
+	def sendCommand(self, command):
+		self.hasReceivedResponse = False
+		self.port.write(command.encode(DEFAULT_ENCODING) + DEFAULT_EOF.encode(DEFAULT_ENCODING))
+		#self.port.flush()
+		while (self.hasReceivedResponse == False):
+			time.sleep(0.001)
+		return self.response.decode(DEFAULT_ENCODING)
+
+#----------------------------------------------------------------------
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Photography Parallax Head Motorization
 This project controls stepper motors and a camera in order to take 360 deg.
 Panoramic Photographs automatically.
 
-Dependencies
+Arduino Dependencies
 -------------
 
 The code controls an Arduino open source hardware Motorshield and requires
@@ -23,4 +23,22 @@ git clone git://github.com/adafruit/AccelStepper.git AccelStepper
 - https://github.com/interactive-matter/aJson
 <pre>
 git clone git://github.com/interactive-matter/aJson.git aJSON
+</pre>
+
+Python / Gphoto2
+-------------
+
+In order to run gphoto2 on mac we use Homebrew
+make sure you have xcode and the xcode command line tools installed in order to
+successfully install ghpoto2 via brew!
+
+<pre>
+	https://github.com/mxcl/homebrew
+	/usr/bin/ruby -e "$(/usr/bin/curl -fksSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)"
+	brew install gphoto2
+</pre>
+
+<pre>
+	http://pyserial.sourceforge.net/
+	python setup.py install
 </pre>
